5f38406358a06f10bd6540bec710924d4292ab41
suzyahyah <suzyahyah@gmail.com>
2018-11-15 03:09:46 -0500

    clean up indexes, handle EOS, remove some unnecessary dictionaries, sanity check

--- vtag.py
+++ vtag.py
@@ -7,6 +7,7 @@ Assignment: HW 6 -- HMMs
 '''
 
 import sys
+import pdb
 from collections import defaultdict
 
 class HMM:
@@ -52,14 +53,21 @@ class HMM:
                     num_sentences += 1
                     SOS = False
                 
+                # Unsmoothed version. If tag has not been seen in training set will assign
+                # 0 prob
+                # Hardcoded for now
                 if word not in self.tag_dict.keys():
-                    self.tag_dict[word] = []
-                self.tag_dict[word].append(tag)
+                    self.tag_dict[word] = ['H', 'C']
+                    #self.tag_dict[word] = []
+                
+                #if tag not in self.tag_dict[word]:
+                #    self.tag_dict[word].append(tag)
+                
 
-                if word not in self.observations:
-                    self.observations.add(word)
-                if tag not in self.states:
-                    self.states.add(tag)
+                #if word not in self.observations:
+                #    self.observations.add(word)
+                #if tag not in self.states:
+                #    self.states.add(tag)
                 
                 if prev_tag not in transition_counts:
                     transition_counts[prev_tag] = defaultdict(int)
@@ -76,67 +84,101 @@ class HMM:
             for tag in start_counts.keys():
                 self.start_probs[tag] = float(start_counts[tag]) / num_sentences
 
-            for tag in transition_counts:
-                self.transition_probs[tag] = {}
-                transition_sum = sum(transition_counts[tag].values())
-                for prev_tag in transition_counts[tag]:
-                    self.transition_probs[tag][prev_tag] = \
-                            float(transition_counts[tag][prev_tag]) / transition_sum
+            for prev_tag in transition_counts:
+                self.transition_probs[prev_tag] = {}
+                transition_sum = sum(transition_counts[prev_tag].values())
 
-            for word in emission_counts:
-                self.emission_probs[word] = {}
-                emission_sum = sum(emission_counts[word].values())
-                for tag in emission_counts[word]:
-                    self.emission_probs[word][tag] = \
-                            float(emission_counts[word][tag]) / emission_sum
+                for tag in transition_counts[prev_tag]:
 
-        print("transition probabilities:", self.transition_probs)
-        print("emission probabilities:", self.emission_probs)
-        print("start probabilities:", self.start_probs)
+                    self.transition_probs[prev_tag][tag] = \
+                            float(transition_counts[prev_tag][tag]) / transition_sum
+            
+            
+            #for word in emission_counts:
+            #    self.emission_probs[word] = {}
+            #    emission_sum = sum(emission_counts[word].values())
+            #    for tag in emission_counts[word]:
+            #        self.emission_probs[word][tag] = \
+            #                float(emission_counts[word][tag]) / emission_sum
+
+
+            for tag in emission_counts:
+                self.emission_probs[tag] = {}
+                emission_sum = sum(emission_counts[tag].values())
+                for word in emission_counts[tag]:
+                    self.emission_probs[tag][word] = \
+                            float(emission_counts[tag][word]) / emission_sum
+
+        print("\ntransition probabilities:", self.transition_probs)
+
+        print("\nemission probabilities:", self.emission_probs)
+        #print("\nstart probabilities:", self.start_probs)
+        
         print()
 
 
     def test_and_eval(self, test_file):
         ### VITERBI
         # Based on the algorithm on page R-4 in handout
-        mu_t = [1]
-        prev_word = '###'
-        back_t = ['']
+        #mu_t = [1]
+        mu_t = []
+        prev_word = ''
+        back_t = []
+        #back_t = ['']
 
         true_tags = []
 
         with open(test_file, 'r') as f:
-            f.readline()
 
-            for line_no, line in enumerate(f):
-                i = line_no + 1
+            for i, line in enumerate(f):
+                #i = line_no + 1
                 w_i, tag = line.strip().split('/')
                 true_tags.append(tag)
     
                 # TODO: handle EOS
                 if w_i == "###":
-                    back_t.append("###")
-                    mu_t.append(1)
-                    prev_word = "###"
-                    continue
+                    
+                    # SOS
+                    if prev_word == '':
+                        back_t.append('###')
+                        mu_t.append(1) # the probability of starting is 1
+                        prev_word = "###"
+                        continue
+                    
+                    # EOS
+                    else:
+                        tags = {'###'}
+                        prev_tags = self.tag_dict[prev_word]
                 
-                tags = self.tag_dict[w_i]
-                if prev_word == '###':
-                    prev_tags = self.start_probs.keys()
+                # First word
+                elif prev_word == '###':
+                    tags = self.start_probs.keys()
+                    prev_tags = {'###'} # ### tags ###
+
                 else:
+                    tags = self.tag_dict[w_i]
                     prev_tags = self.tag_dict[prev_word]
                 
                 # initialize mu_t[i] and back_t[i]
                 mu_t.append(0)
                 back_t.append('')
 
+
                 for t_i in tags:
                     for t_im1 in prev_tags:
+                        
                         p = hmm.transition_probs[t_im1][t_i] * hmm.emission_probs[t_i][w_i]
                         mu = mu_t[i-1] * p
+
                         if mu > mu_t[i]:
                             mu_t[i] = mu
-                            back_t[i] = t_im1
+                            back_t[i] = t_i
+
+                # Sanity check against Viterbi excel sheet
+                # running into underflow issues?
+                if (i<15):
+                    print(i, back_t[i], mu_t[i])
+
 
                 prev_word = w_i  
             
@@ -149,7 +191,6 @@ class HMM:
             # reverse array to get sequence of tags in sentence order
             pred_tags = pred_tags[::-1]
             '''
-            del back_t[0]
 
             # compare and evaluate
             correct = 0
