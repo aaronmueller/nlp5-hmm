07f7950488e23e60140eeaf0f14e2defb2c7ae00
suzyahyah <suzyahyah@gmail.com>
2018-11-18 23:14:35 -0500

    EM run overview

--- runEM.py
+++ runEM.py
@@ -5,19 +5,24 @@ import sys
 import vtagem
 import pdb
 
-numIterations = 1
+numIterations = 10
 
 hmm = vtagem.HMM(sys.argv[1], sys.argv[2], sys.argv[3])
 hmm.train(sys.argv[1]) # Initialisation of model parameters
-#hmm.prepare_for_em()
-hmm.doMStep()
+hmm.collect_test_statistics()
+hmm.collect_raw_statistics()
+hmm.initialise_on_train()
 hmm.test_and_eval()
 #pdb.set_trace()
 
-#for itr in range(numIterations):
-#    hmm.doEStep() # compute new counts on raw
-#    hmm.doMStep() # resetimate model parameters
-#    hmm.test_and_eval() # recode test data
+for itr in range(numIterations):
+    print("Iteration:"+str(itr))
+    print("Estep..")
+    hmm.doEStep() # compute new counts on raw
+    print("Mstep..")
+    hmm.doMStep() # resetimate model parameters
+    print("Test eval..")
+    hmm.test_and_eval() # recode test data
 
 
 
