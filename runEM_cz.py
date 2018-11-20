4394ea65d6922a84f9e4aa68b443e55a63158841
suzyahyah <suzyahyah@gmail.com>
2018-11-20 02:16:58 -0500

    add timer

--- runEM_cz.py
+++ runEM_cz.py
@@ -4,10 +4,11 @@
 import sys
 import vtagem_cz
 import pdb
+import time
 
 numIterations = 10
 
-hmm = vtagem.HMM(sys.argv[1], sys.argv[3], sys.argv[2])
+hmm = vtagem_cz.HMM(sys.argv[1], sys.argv[3], sys.argv[2])
 hmm.train(sys.argv[1]) # Initialisation of model parameters
 hmm.collect_raw_statistics()
 hmm.collect_test_statistics()
@@ -16,10 +17,13 @@ hmm.test_and_eval()
 #pdb.set_trace()
 
 for itr in range(numIterations+1):
+    start = time.time()
     print("Iteration "+str(itr)+":",end="\t")
     hmm.doEStep() # compute new counts on raw
     hmm.doMStep() # resetimate model parameters
     hmm.test_and_eval() # recode test data
+
+    print("Time Elapsed:", time.time() - start)
 
 
 
